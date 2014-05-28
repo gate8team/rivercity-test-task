a5b0269fdd6bf9edbc7801aa4a368f4e5d1e21af
ashabanov <me.antony.rain@gmail.com>
2014-05-28 20:37:14 +0400

    [INFO]: initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            build: {
+                src: ['static/js/dev/gate8team.js'],
+                dest: 'static/js/gate8team.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
